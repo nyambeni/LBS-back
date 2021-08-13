d779e4e1a009c7b5e6956580248e6aa4a083816d
Godfrey1234 <godfrey555mabena@gmail.com>
2021-08-13 01:03:11 -0700

    added code for sending emails on forgot password

--- routes/forgot_Password.js
+++ routes/forgot_Password.js
@@ -6,21 +6,52 @@ exports.forgotPassword =async function(request, response)
     var stuNumber = request.body.stuNumber;
     var email = request.body.email;
     
-    //console.log(stuNumber);
+    console.log(stuNumber);
     console.log(email);
 
     
-    connection.query('SELECT password FROM student where email = ?', [email], function(error, results, fields)  {
+    connection.query('SELECT password FROM student where stud_no = ?', [stuNumber], function(error, results, fields)  {
     
         if (results.length > 0){
 
             var pas = results//this string returns the users password
             response.send(JSON.stringify(pas));
             //res.send(JSON.stringify(pas));
+           
             //write a code to send email with that pas string
+            var nodemailer = require('nodemailer')
+            var transporter = nodemailer.createTransport({
+            
+                service:'gmail',
+                auth:{
+                    user:'godfrey555mabena@gmail.com',
+                    pass:'godfreyzo'
+                    
+                }
 
 
+            });
 
+            var mailOptions ={
+
+                from:'godfrey555mabena@gmail.com',
+                to:JSON.stringify(email),
+                subject:'lab booking system password',
+                text: JSON.stringify(pas)
+
+            };
+
+
+            transporter.sendMail(mailOptions,function(error,info){
+
+                if(error){
+                    console.log(error)
+                }else{
+                    console.log('Email sent ' + info.response)
+                }
+
+
+            })
 
 
             
